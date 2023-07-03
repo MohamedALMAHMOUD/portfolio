47f7147023b8954b7762308faf458d04ad57ee4a
Mohamed ALMAHMOUD <contact@cabane-maths.fr>
2023-07-03 15:00:05 +0200

    add js type machine

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 // script.js
 
 // Ajoutez votre code JavaScript ici
+const textElement = document.getElementById('typewriter-text');
+const text = "Bienvenue sur mon portfolio.";
+let index = 0;
+
+function type() {
+  if (index < text.length) {
+    textElement.innerHTML += text.charAt(index);
+    index++;
+    setTimeout(type, 50); // Vitesse de frappe, ajustez-la selon vos préférences
+  }
+}
+
+type();
+
