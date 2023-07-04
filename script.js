df17635ea52faedab46b323edbc2c31ca466d18a
Mohamed ALMAHMOUD <contact@cabane-maths.fr>
2023-07-04 23:05:35 +0200

    update

--- script.js
+++ script.js
@@ -14,41 +14,21 @@ function typeWriter() {
 }
 
 typeWriter();
-const $ = function (selector, bind) {
-	var bind = bind === undefined ? document : bind;
-	let nodes = bind.querySelectorAll.bind(bind)(selector);
-	return nodes.length == 1 ? nodes[0] : nodes;
-};
-var textarea = $(".editor");
+var textarea = document.querySelector(".editor");
 var editor = CodeMirror.fromTextArea(textarea, {
 	theme: "berry-dark",
-	autoRefresh: true,
-	firstLineNumber: 1,
 	lineNumbers: true,
-	smartIndent: true,
-	lineWrapping: true,
+	indentUnit: 4,
 	indentWithTabs: true,
-	refresh: true
 });
-editor.removeTag = CodeMirror.removeTag;
-var cm = $(".CodeMirror");
-cm.editor = editor;
-editor.save();
-editor.setOption("mode", "htmlmixed");
 
-$(".copy-code-wrap").onclick = function (e) {
-	if (e.which == 1) {
-		// write the text to the clipboard
-		navigator.clipboard.writeText(editor.getValue());
+document.querySelector(".copy-code-wrap").onclick = function() {
+	navigator.clipboard.writeText(editor.getValue());
 
-		// animate the button
-		var copy = $(".copy-code", this);
-		function quickadd() {
-			copy.classList.add("animate");
-			setTimeout(function () {
-				copy.classList.remove("animate");
-			}, 200);
-		}
-		quickadd();
-	}
+	var copy = document.querySelector(".copy-code");
+	copy.classList.add("animate");
+	setTimeout(function () {
+		copy.classList.remove("animate");
+	}, 200);
 };
+
