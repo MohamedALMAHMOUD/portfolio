a43bc61a38a3a909b0b5031393a7e7aa4652a434
Mohamed ALMAHMOUD <contact@cabane-maths.fr>
2023-07-05 11:36:48 +0200

    update

--- script.js
+++ script.js
@@ -15,40 +15,17 @@ function typeWriter() {
 
 typeWriter();
 
-  var editor = CodeMirror.fromTextArea(document.getElementById("code-editor"), {
-    lineNumbers: true,  // Afficher les numéros de ligne
-    mode: "text/html",  // Définir le mode de coloration syntaxique (HTML, CSS, JavaScript, etc.)
-    theme: "default"  // Choisir un thème pour l'éditeur
-    // Autres options que vous souhaitez configurer
-  });
-  var form_copier_coller = document.getElementById("form-copier-coller")
-  // On sélectionne le <textarea>
-  var textarea_texte = document.getElementById("texte")
-
-  // Copier le texte
-  copierTexte = (e) => {
-      // On désactive l'action du formulaire
-      e.preventDefault()
-      // 1. Si le <textarea> n'est pas vide
-      if (textarea_texte.value.length) {
-          // 2. On copie le texte dans le presse-papier
-          navigator.clipboard.writeText(textarea_texte.value).then(() => {
-              // 3. On réinitialise le formulaire
-              form_copier_coller.reset()
-              // 4. On affiche l'alert
-              alert("Texte copié !")
-          })
-      } else {
-          alert("Veillez saisir le texte à copier")
-      }
-  }
-  // Coller le texte sera inséré ici
-
-// Coller le texte
-collerTexte = (e) => {
-  e.preventDefault()
-  // On lit le contenu du clipboad qu'on affecte au <textarea>
-  navigator.clipboard.readText().then((texte) => {
-      textarea_texte.value = texte
-  })
-}
+let editor = document.querySelector("textarea");
+let button = document.querySelector("button");
+button.addEventListener('click', () => {
+  console.log('Je suis cliqué');
+});
+button.addEventListener('click', () => {
+  editor.select();
+  button.innerText = "Copié !";
+});
+button.addEventListener('click', () => {
+  editor.select();
+  document.execCommand("copy");
+  button.innerText = "Copié !";
+});
