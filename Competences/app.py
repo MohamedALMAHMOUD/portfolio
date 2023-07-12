c5efe7e1d436cc7e8c69b351b37ef0f76b103022
Mohamed ALMAHMOUD <contact@cabane-maths.fr>
2023-07-12 19:33:00 +0200

    update host flask

--- Competences/app.py
+++ Competences/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def index():
   return render_template('index.html')
 
-@app.route('/index', methods=['POST', 'GET'])
+@app.route('/index', methods=['POST'])
 def result():
     output = request.form.to_dict()
     mdp = output['mdp']
@@ -16,4 +16,4 @@ def result():
     return render_template('index.html', resultat=resultat)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='127.0.0.1', port=5000)
