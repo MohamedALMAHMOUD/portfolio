93bc96ca2a37e64d847568dc99b5ee0154d1f1df
Mohamed ALMAHMOUD <contact@cabane-maths.fr>
2023-07-12 14:07:33 +0200

    update différents files

--- script.js
+++ script.js
@@ -2,17 +2,49 @@
 
 // Ajoutez votre code JavaScript ici
 const textElement = document.querySelector('.type-machine');
-const textToType = "Je suis fasciné par l'informatique, autodidacte et je me forme en permanence en Python, SQL et Machine Learning. J'aime le nouveau chalenge et je suis ouvert à toute proposition dans le domaine de la data. Je suis quelqu'un très motivé et force de propositions, j'aime l'échange et le travail en équipe.";
+const textToType = textElement.getAttribute('data-text');
 let index = 0;
 
 function typeWriter() {
-  if (index < textToType.length) {
-    textElement.textContent += textToType.charAt(index);
-    index++;
-    setTimeout(typeWriter, 50); // Vitesse de défilement du texte (en millisecondes)
+  if (textElement) {
+    if (index < textToType.length) {
+      textElement.textContent += textToType.charAt(index);
+      index++;
+      setTimeout(typeWriter, 50); // Vitesse de défilement du texte (en millisecondes)
+    }
   }
 }
 
 typeWriter();
 
+// carousel
+let slideIndex = 0;
 
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }    
+  
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  
+  if (slides[slideIndex - 1]) {
+    slides[slideIndex - 1].style.display = "block";  
+    dots[slideIndex - 1].className += " active";
+  }
+  
+  setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
+
+
+showSlides();
