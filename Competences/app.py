0fb5789b5716f57548e703edbe2bb4a83866d0ee
Mohamed ALMAHMOUD <contact@cabane-maths.fr>
2023-07-16 22:44:53 +0200

    new try to deploy flask

--- Competences/app.py
+++ Competences/app.py
@@ -15,12 +15,12 @@ def result():
   mdp = output['mdp']
   resultat = passeword(mdp)
   return render_template('index.html', resultat=resultat)
-  wsgi_app = app.wsgi_app
-  if __name__ == '__main__':
-    import os
-    HOST = os.environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(os.environ.get('SERVER_PORT', '5555'))
-    except ValueError:
-        PORT = 5555
-    app.run(HOST, PORT)
+wsgi_app = app.wsgi_app
+if __name__ == '__main__':
+  import os
+  HOST = os.environ.get('SERVER_HOST', 'localhost')
+  try:
+      PORT = int(os.environ.get('SERVER_PORT', '5555'))
+  except ValueError:
+      PORT = 5555
+  app.run(HOST, PORT)
